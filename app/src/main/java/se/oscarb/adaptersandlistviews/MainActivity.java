0aa0b97dac2a199e8c2bec8b72771161f47c5721
Oscar <github@oscarb.se>
2016-02-23 14:31:00 +0100

    Added more cars, a MyCustomAdapter and optimized the getView method inside the custom adapter

--- app/src/main/java/se/oscarb/adaptersandlistviews/MainActivity.java
+++ app/src/main/java/se/oscarb/adaptersandlistviews/MainActivity.java
@@ -2,8 +2,12 @@ package se.oscarb.adaptersandlistviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,19 +29,43 @@ public class MainActivity extends AppCompatActivity {
         cars.add("Toyota");
         cars.add("Volvo");
         cars.add("Mercedes");
+        cars.add("Ferrari");
+        cars.add("BMW");
+        cars.add("Lamborghini");
+        cars.add("Saab");
+        cars.add("Ford");
+        cars.add("Chevrolet");
+        cars.add("Cadillac");
+        cars.add("Chrysler");
+        cars.add("Dodge");
+        cars.add("Jeep");
+        cars.add("Fiat");
+        cars.add("Renault");
+        cars.add("Nissan");
+        cars.add("Peugeot");
+        cars.add("Honda");
+        cars.add("Lada");
+
+
 
 
         // Hämta vår ListView
         ListView myListView = (ListView) findViewById(R.id.my_list_view);
 
-        // Skapa vår adapter
-        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
+        // Skapa adapter med egen TextView
+        ArrayAdapter<String> myBuiltInArrayAdapter = new ArrayAdapter<>(this,
+                R.layout.my_text_view,
                 cars);
 
-        // Använd vår adapter
-        myListView.setAdapter(myArrayAdapter);
+        // Eller
 
+        // Skapa anpassad adapter med egen layout
+        MyCustomAdapter myCustomAdapter = new MyCustomAdapter(this, R.layout.my_text_view, cars);
+
+        // Använd vår adapter
+        //myListView.setAdapter(myBuiltInArrayAdapter);
+
+        myListView.setAdapter(myCustomAdapter);
 
     }
 }
